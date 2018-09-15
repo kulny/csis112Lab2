80a61170087d29e1467f83c1a8028b52f81c9bf9
Cody Moore <cmoore109@liberty.edu>
2018-09-15 17:05:37 -0400

    isalpha checking?

--- csis112Lab2/RectangleDriver.cpp
+++ csis112Lab2/RectangleDriver.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <string>
 #include "Rectangle.h"
+#include <cctype>
 
 
 using namespace std;
@@ -58,18 +59,40 @@ bool EndOfInputStream(istream &input) // checks if ctrl z input
 double InputLoop(string s)
 {
 	double d;
+	string line;
 
 	do
 	{
 		cout << "Please input the " << s << " of the Rectangle." << endl;
-		cin >> d;
+		//cin >> d;
+		getline(cin, line, '\n');
+		for (int i = 0; i < line.length; i++)
+		{
+			
+			if (isalpha(line[i]))
+			{
+				cout << "alphabet";
+			}
+		}
+		try
+		{
+			stod(line);
+		}
+		catch (const invalid_argument)
+		{
+			cout << "You cannot have letters in the input.";
+		}
+		catch (const out_of_range)
+		{
+			cout << "You cannot have letters in the input.";
+		}
 		if (EndOfInputStream(cin))
 		{
 			exit(0);
 		}
-	} while (!InputFilter(d));
+	} while (!InputFilter(stod(line))); // loops the input query if error given
 	 
-	return d;
+	return stod(line);
 }
 
 bool InputFilter(double in)
@@ -79,8 +102,6 @@ bool InputFilter(double in)
 		cout << "You must enter a number, and that number must be positive.  Please try again. " << endl;
 		cin.clear();
 		cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		/*cout << "\nEnter a number (Ctrl-Z to exit):  ";
-		cin >> in;*/
 		return false;
 	}
 	else
